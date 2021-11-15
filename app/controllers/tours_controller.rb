5b12e85523ddc0a5d47c3351c51c8d7fe1bcaf46
github username <rvtingling@yahoo.com>
2021-11-15 10:58:16 -0500

    created controller actions for tours

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,2 +1,47 @@
 class ToursController < ApplicationController
+
+    def index
+        @tours = Tour.all
+    
+        render json: @tours
+    end
+
+    def show
+        @tour = Tour.find_by_id(params[:id])
+        render json: @tour
+    end
+
+    def create
+        @tour = Tour.new(tour_params)
+    
+        if @tour.save
+          render json: @tour, status: :created, location: @tour
+        else
+          render json: @tour.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @tour = Tour.find(params[:id])
+        if @tour.update(tour_params)
+          render json: @tour
+        else
+          render json: @tour.errors, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @tour = Tour.find(params[:id])
+        @tour.destroy
+    end
+    
+    private
+       
+    def set_tour
+        @tour = Tour.find(params[:id])
+    end
+    
+    def tour_params
+        params.require(:tour).permit(:name, :duration, :location, :image, :cost, :description)
+    end
 end
