130227d449a570978d1d275f38522f68edf3daf2
github username <rvtingling@yahoo.com>
2021-11-17 01:43:44 -0500

    update controller actions for reservations and users

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,45 @@
 class ReservationsController < ApplicationController
+
+    def index
+        reservations = Reservation.all 
+        render json: reservations
+        # render json: ReservationSerializer.new(reservations)
+    end 
+
+    def show 
+        @reservation = Reservation.find_by_id(params[:id])
+        render json: @reservation
+    end 
+
+    def create 
+        reservation = Reservation.new(reservation_params)
+        if reservation.save 
+            render json: reservation
+            # render json: ReservationSerializer.new(reservation)
+        else 
+            render json: {errors: reservation.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    def destroy 
+        reservation = Reservation.find_by_id(params[:id])
+        if reservation.destroy
+            reservations = Reservation.all 
+            render json: reservations
+            # render json: ReservationSerializer.new(reservations)
+        else
+            render json: { message: "error"}  
+        end 
+    end 
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:desired_date, :desired_time, :special_request, :tour_id, :user_id)
+    end 
+
+    def find_reservation
+        @reservation = Reservation.find_by_id(params[:id])
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all 
+        render json: users
+    end 
+
+    def show 
+        @user = User.find_by_id(params[:id])
+        render json: @user
+    end 
+    
+    def create 
+        user = User.new(user_params)
+        if user.save 
+            render json: user
+            # render json: UserSerializer.new(user)
+        else 
+            render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    def destroy 
+        user = User.find_by_id(params[:id])
+        if user.destroy
+            users = User.all 
+            render json: users
+            # render json: UserSerializer.new(users)
+        else
+            render json: { message: "error"}  
+        end 
+    end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :phone, :password)
+    end 
+
+    def find_user
+        @user = User.find_by_id(params[:id])
+    end
 end
